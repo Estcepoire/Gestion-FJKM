cebce4295969d418400a290d0dd7a3d17d0f75dc
Mamison Rajaoharisaona <rajaoharisaona@gmail.com>
2024-09-21 20:48:03 +0300

    [dev][MvtStock]

--- demo-ejb/src/java/stock/MvtStock.java
+++ demo-ejb/src/java/stock/MvtStock.java
@@ -1,12 +1,73 @@
 package stock;
 
+import java.sql.Connection;
 import java.sql.Date;
 import bean.*;
-public class MvtStock  {
-    String id, designation, idMagasin, idConsommation, idTransfert, idTypeMvStock;
-    Date daty;
-    
-        
 
-    
+public class MvtStock extends ClassMere {
+    String id, designation, idMagasin, idTypeMvStock;
+    Date daty;
+
+    public MvtStock() throws Exception {
+        this.setNomTable("MvtStock");
+        this.setLiaisonFille("idMere");
+        this.setNomClasseFille("stock.MvtStockFille");
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public String getIdMagasin() {
+        return idMagasin;
+    }
+
+    public void setIdMagasin(String idMagasin) {
+        this.idMagasin = idMagasin;
+    }
+
+    public String getIdTypeMvStock() {
+        return idTypeMvStock;
+    }
+
+    public void setIdTypeMvStock(String idTypeMvStock) {
+        this.idTypeMvStock = idTypeMvStock;
+    }
+
+    public Date getDaty() {
+        return daty;
+    }
+
+    public void setDaty(Date daty) {
+        this.daty = daty;
+    }
+
+    @Override
+    public String getAttributIDName() {
+        return "id";
+    }
+
+    @Override
+    public String getTuppleID() {
+        return this.getId();
+    }
+
+    @Override
+    public void construirePK(Connection c) throws Exception {
+        this.preparePk("MVTSTOCK", "GET_SEQMVTSTOCK");
+        this.setId(makePK(c));
+    }
+
 }
--- demo-ejb/src/java/stock/MvtStockFille.java
+++ demo-ejb/src/java/stock/MvtStockFille.java
@@ -1,6 +1,97 @@
 package stock;
-import bean.*;
 
-public class MvtStockFille {
-    
+import bean.ClassFille;
+
+public class MvtStockFille extends ClassFille {
+    String id, idMere, idProduit;
+    String val, desce, remarque;
+    double entre, sortie, quantite;
+
+    public String getIdProduit() {
+        return idProduit;
+    }
+
+    public void setIdProduit(String idProduit) {
+        this.idProduit = idProduit;
+    }
+
+    public String getVal() {
+        return val;
+    }
+
+    public void setVal(String val) {
+        this.val = val;
+    }
+
+    public String getDesce() {
+        return desce;
+    }
+
+    public void setDesce(String desce) {
+        this.desce = desce;
+    }
+
+    public String getRemarque() {
+        return remarque;
+    }
+
+    public void setRemarque(String remarque) {
+        this.remarque = remarque;
+    }
+
+    public double getEntre() {
+        return entre;
+    }
+
+    public void setEntre(double entre) {
+        this.entre = entre;
+    }
+
+    public double getSortie() {
+        return sortie;
+    }
+
+    public void setSortie(double sortie) {
+        this.sortie = sortie;
+    }
+
+    public double getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(double quantite) {
+        this.quantite = quantite;
+    }
+
+    public MvtStockFille() {
+        this.setNomTable("MvtStockFille");
+        this.setLiaisonMere("idMere");
+    }
+
+    @Override
+    public String getAttributIDName() {
+        return "id";
+    }
+
+    @Override
+    public String getTuppleID() {
+        return this.getId();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIdMere() {
+        return idMere;
+    }
+
+    public void setIdMere(String idMere) {
+        this.idMere = idMere;
+    }
+
 }
