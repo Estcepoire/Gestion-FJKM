835a2af13a5b7e2d225e29f85db598c8c0bf07c9
ManoarySarobidy <manoarysarobidy@gmail.com>
2024-10-13 20:51:32 +0300

    Update addLine.js

--- demo-war/web/apjplugins/addLine.js
+++ demo-war/web/apjplugins/addLine.js
@@ -1,31 +1,77 @@
-
-
-function add_line_tab(champ){
-    var firstElement = document.getElementById("ajout_multiple_ligne").lastChild.innerHTML;
+function add_line_tab(champ) {
+    var lastRow = document.getElementById("ajout_multiple_ligne").lastChild;
+    
+    var newRow = lastRow.cloneNode(true);
+    
     var indiceNew = $("#ajout_multiple_ligne tr").length;
-    console.log("firstElement",firstElement)
-    //var newElem = firstElement.toString().replaceAll(/(?<=[^0-9])0(?=[^0-9])/g, ''+indiceNew);
-    var newElem = firstElement.replaceAll(/[0-9]/g, indiceNew);
-    newElem = newElem.replaceAll(indiceNew+""+indiceNew, indiceNew);
-    newElem = newElem.replaceAll(indiceNew+""+indiceNew+""+indiceNew, indiceNew);
-    newElem = newElem.replaceAll(indiceNew+""+indiceNew+""+indiceNew+""+indiceNew, indiceNew);
-    console.log("newElen",newElem)
-    $("#ajout_multiple_ligne").append("<tr id='ligne-multiple-"+indiceNew+"'>"+newElem+"</tr>");
-    $("#nombreLigne").val(indiceNew+1);
+    updateElementIds(newRow, indiceNew);
+    
+    copyElementValues(lastRow, newRow);
+    
+    document.getElementById("ajout_multiple_ligne").appendChild(newRow);
+    
+    $("#nombreLigne").val(indiceNew + 1);
+    document.getElementById("checkbox"+indiceNew).value = indiceNew;
 }
 
-function add_line_tabs(champ){
-    var firstElement = document.getElementById("ajout_multiple_ligne").lastChild.innerHTML;
-    for(let i=0;i<10;i++){
-        var indiceNew = $("#ajout_multiple_ligne tr").length;
-        //var newElem = firstElement.replaceAll(/(?<=[^0-9])0(?=[^0-9])/g, (indiceNew+i));
-        var newElem = firstElement.replaceAll(/[0-9]/g, indiceNew);
-        newElem = newElem.replaceAll(indiceNew+""+indiceNew, indiceNew);
-        newElem = newElem.replaceAll(indiceNew+""+indiceNew+""+indiceNew, indiceNew);
-        newElem = newElem.replaceAll(indiceNew+""+indiceNew+""+indiceNew+""+indiceNew, indiceNew);
-        $("#ajout_multiple_ligne").append("<tr id='ligne-multiple-"+(indiceNew)+"'>"+newElem+"</tr>");
-        $("#nombreLigne").val(indiceNew+(1));
+function confirmDeleteLine() {
+    return confirm('Voulez-vous vraiment supprimer cet element ?');
+}
+
+function updateElementIds(element, newIndex) {
+    
+    var allElements = element.querySelectorAll("*");
+    
+    allElements.forEach(function (elem) {
+        var currentId = elem.getAttribute("id");
+        if (currentId) {
+            var newId = currentId.replace(/\d+$/, newIndex);
+            elem.setAttribute("id", newId);
+        }
+        var currentName = elem.getAttribute("name");
+        if (currentName) {
+            var newName = currentName.replace(/\d+$/, newIndex);
+            elem.setAttribute("name", newName);
+        }
+        var currentClick = elem.getAttribute("onclick");
+        if (currentClick) {
+            var newClick = currentClick.replace(/\d+/g,newIndex);
+            elem.setAttribute("onclick", newClick);
+        }
+    });
+    
+    element.setAttribute("id","ligne-multiple-"+newIndex);
+}
+
+function copyElementValues(sourceRow, targetRow) {
+    
+    var sourceFormElements = sourceRow.querySelectorAll("*");
+    var targetFormElements = targetRow.querySelectorAll("*");
+    
+    for (var i = 0; i < sourceFormElements.length; i++) {
+        var sourceElement = sourceFormElements[i];
+        var targetElement = targetFormElements[i];
+        
+        if (sourceElement.tagName.toLowerCase() === targetElement.tagName.toLowerCase()) {
+            targetElement.value = sourceElement.value;
+        }
     }
 }
 
-
+function add_line_tabs(champ) {
+    for (let i = 0; i < 10; i++) {
+        var lastRow = document.getElementById("ajout_multiple_ligne").lastChild;
+        
+        var newRow = lastRow.cloneNode(true);
+        
+        var indiceNew = $("#ajout_multiple_ligne tr").length;
+        updateElementIds(newRow, indiceNew);
+        
+        copyElementValues(lastRow, newRow);
+        
+        document.getElementById("ajout_multiple_ligne").appendChild(newRow);
+        
+        $("#nombreLigne").val(indiceNew + 1);
+        document.getElementById("checkbox"+indiceNew).value = indiceNew;
+    }
+}
