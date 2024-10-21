72b32694ecfc536131e420d2993d657ad7b50a08
ManoarySarobidy <manoarysarobidy@gmail.com>
2024-10-21 18:57:12 +0300

    [Metier] - Création classe type Evenement

---
+++ demo-ejb/src/java/evenement/TypeEvenement.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package evenement;
+
+import bean.TypeObjet;
+
+/**
+ *
+ * @author sarobidy
+ */
+public class TypeEvenement extends TypeObjet {
+          
+          public TypeEvenement(){
+                    this.setNomTable("typeevenement");
+          }
+          
+}
